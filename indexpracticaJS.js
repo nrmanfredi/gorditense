59feb7fc2efea7c236ae569b034e88dec59f03b7
nrmanfredi <99990171+nrmanfredi@users.noreply.github.com>
2023-03-27 23:12:53 -0300

    Add files via upload

---
+++ indexpracticaJS.js
@@ -0,0 +1,12 @@
+alert("que ondrag");
+
+const atri = document.querySelector(".paJS");
+
+valorAtributo = atri.setAttribute("color", "red");
+
+const titulito = document.querySelector (".paraJS");
+
+titulito.addEventListener ("click",saludar);
+function saludar() {
+    alert("que??")
+};
